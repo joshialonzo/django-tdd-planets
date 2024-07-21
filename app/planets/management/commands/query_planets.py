29d7cc58929b2e0de4088c648a4801464eabd29c
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2024-07-21 13:44:12 -0600

    Querying planets

---
+++ app/planets/management/commands/query_planets.py
@@ -0,0 +1,36 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Populate the database with planets"
+    url = "https://swapi-graphql.netlify.app/.netlify/functions/index"
+    string = "query Query {allPlanets{planets{name population terrains climates}}}"
+
+    def init_gql_client(self, url):
+        transport = AIOHTTPTransport(url=url)
+        return Client(transport=transport, fetch_schema_from_transport=True)
+
+    def execute_query(self, client):
+        query = gql(self.string)
+        return client.execute(query)
+    
+    def get_planets(self):
+        client = self.init_gql_client(self.url)
+        result = self.execute_query(client)
+        return result["allPlanets"]["planets"]
+
+    def print_planets(self, planets):
+        for planet in planets:
+            planet_name = planet["name"]
+            self.stdout.write(self.style.SUCCESS(f"Planet: {planet_name}"))
+
+    def handle(self, *args, **options):
+        self.stdout.write("Querying planets...")
+
+        planets = self.get_planets()
+        self.print_planets(planets)
+
+        self.stdout.write("Done!")
---
+++ readme.md
@@ -0,0 +1,6 @@
+## References
+
+* [GraphQL Clients](https://graphql.org/graphql-js/graphql-clients/)
+* [GraphQL Clients with Python](https://graphql.org/community/tools-and-libraries/?tags=python)
+* [Python GraphQL Client: GQL](https://github.com/graphql-python/gql)
+* [How to create custom django-admin commands](https://docs.djangoproject.com/en/5.0/howto/custom-management-commands/)
