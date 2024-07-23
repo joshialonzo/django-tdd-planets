1a64af44f2ccb56172e70648adbf27c38a7753e8
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2024-07-23 01:03:11 -0600

    Update a planet

--- app/planets/views.py
+++ app/planets/views.py
@@ -33,6 +33,14 @@ class PlanetDetail(APIView):
         serializer = PlanetSerializer(planet)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        planet = self.get_object(pk)
+        serializer = PlanetSerializer(planet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         planet = self.get_object(pk)
         planet.delete()
--- app/tests/planets/test_views.py
+++ app/tests/planets/test_views.py
@@ -103,3 +103,47 @@ def test_remove_planet(client, add_planet):
 def test_remove_planet_incorrect_id(client):
     resp = client.delete(f"/api/planets/99/")
     assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_planet(client, add_planet):
+    planet = add_planet(name="Earth", population=8_100_000_000)
+
+    resp = client.put(
+        f"/api/planets/{planet.id}/",
+        {"name": "Earth", "population": 8_300_000_000},
+        content_type="application/json"
+    )
+    assert resp.status_code == 200
+    assert resp.data["name"] == "Earth"
+    assert resp.data["population"] == 8_300_000_000
+
+    resp_two = client.get(f"/api/planets/{planet.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["name"] == "Earth"
+    assert resp.data["population"] == 8_300_000_000
+
+
+@pytest.mark.django_db
+def test_update_planet_incorrect_id(client):
+    resp = client.put(f"/api/planets/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_planet_invalid_json(client, add_planet):
+    planet = add_planet(name="Earth", population=8_100_000_000)
+    resp = client.put(f"/api/planets/{planet.id}/", {}, content_type="application/json")
+    assert resp.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_planet_invalid_json_keys(client, add_planet):
+    planet = add_planet(name="Earth", population=8_100_000_000)
+
+    resp = client.put(
+        f"/api/planets/{planet.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 400
